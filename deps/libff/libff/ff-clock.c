670f7773b4570d41999bb66ea2bc70d9b9a24f3f
John Bradley <jrb@turrettech.com>
2015-03-12 15:24:15 -0500

    deps-libff: Add reference counting to clock

--- deps/libff/libff/ff-clock.c
+++ deps/libff/libff/ff-clock.c
@@ -16,7 +16,63 @@
 
 #include "ff-clock.h"
 
+#include <libavutil/avutil.h>
 double ff_get_sync_clock(struct ff_clock *clock)
 {
 	return clock->sync_clock(clock->opaque);
+}
+struct ff_clock *ff_clock_init(struct ff_clock *clock)
+{
+	clock = av_mallocz(sizeof(struct ff_clock));
+
+	if (clock == NULL)
+		return NULL;
+
+	if (pthread_mutex_init(&clock->mutex, NULL) != 0)
+		goto fail;
+
+	if (pthread_cond_init(&clock->cond, NULL) != 0)
+		goto fail1;
+
+	return clock;
+fail1:
+	pthread_mutex_destroy(&clock->mutex);
+fail:
+	av_free(clock);
+
+	return NULL;
+}
+
+struct ff_clock *ff_clock_retain(struct ff_clock *clock)
+{
+	pthread_mutex_lock(&clock->mutex);
+	clock->retain++;
+	pthread_mutex_unlock(&clock->mutex);
+
+	return clock;
+}
+
+struct ff_clock *ff_clock_move(struct ff_clock **clock)
+{
+	struct ff_clock *retained_clock = ff_clock_retain(*clock);
+	ff_clock_release(clock);
+
+	return retained_clock;
+}
+
+void ff_clock_release(struct ff_clock **clock)
+{
+	bool release = false;
+	pthread_mutex_lock(&(*clock)->mutex);
+	(*clock)->retain--;
+	release = (*clock)->retain == 0;
+	pthread_mutex_unlock(&(*clock)->mutex);
+
+	if (release) {
+		pthread_cond_destroy(&(*clock)->cond);
+		pthread_mutex_destroy(&(*clock)->mutex);
+		av_free(*clock);
+	}
+
+	*clock = NULL;
 }
