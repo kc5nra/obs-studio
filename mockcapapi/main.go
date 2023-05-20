df0c410064e2009ee9463f0c06b72fec67ceb884
Andrew Francis <andrewf@twitch.tv>
2023-05-19 23:24:45 -0700

    option in the mock api to return an SJC prod config, with no client side encodes

--- mockcapapi/main.go
+++ mockcapapi/main.go
@@ -10,14 +10,23 @@ import _ "embed"
 //go:embed out.json
 var content []byte
 
+//go:embed out-sjcprod.json
+var contentSjcProd []byte
+
 func main() {
   var delayMs int
+  var sjcProd bool
   flag.IntVar(&delayMs, "delayms", 0, "Delay responses by this time (in milliseconds)")
+  flag.BoolVar(&sjcProd, "sjcprod", false, "Point client to sjc05 production ingest, with no video encodings")
   flag.Parse()
 
   http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
     time.Sleep(time.Millisecond * time.Duration(delayMs))
-    res.Write(content)
+    if sjcProd {
+        res.Write(contentSjcProd)
+    } else {
+        res.Write(content)
+    }
   })
   
   log.Fatal(http.ListenAndServe("127.0.0.1:8787", nil))
